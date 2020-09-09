50a4173327846f99d21253d34301c4c79b1c273a
admin <admin@DuyTran166.local>
2020-09-09 22:58:24 +0700

    initial xcode project creation

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by admin on 09/09/2020.
+//  Copyright © 2020 Last Destroyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
